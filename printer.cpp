5ca24932449124066d9c27be9e7cb1adabd92cf4
sovaz1997 <sovaz1997@yandex.ru>
2016-10-27 22:28:48 +0300

    upd. 22:30

--- printer.cpp
+++ printer.cpp
@@ -5,8 +5,8 @@ void Game::printScore(double score) {
 	if(score > BLACK_WIN + 10000 && score < WHITE_WIN - 10000) {
 		std::cout << "cp " << (int) (score / PAWN_EV * 100);
 	} else if(score < 0) {
-		std::cout << "score mate " <<  -abs(score - BLACK_WIN) / 2 - 1;
+		std::cout << "score mate " <<  (int)(-abs(score - BLACK_WIN) / 2 - 1);
 	} else {
-		std::cout << "mate " <<  abs(score - WHITE_WIN) / 2 + 1;
+		std::cout << "mate " <<  (int)(abs(score - WHITE_WIN) / 2 + 1);
 	}
 }
