7f8f4c02f5f50b6d5feb7d3f875fcf2bd032ef9e
sovaz1997 <sovaz1997@yandex.ru>
2016-11-11 13:05:10 +0300

    added option "nullmove"

---
+++ option.cpp
@@ -0,0 +1,9 @@
+#include "option.hpp"
+
+Option::Option() : nullMovePrunningEnable(false) {
+	
+}
+
+void Option::print() {
+	std::cout << "option name nullmove type check default false\n";
+}
---
+++ option.hpp
@@ -0,0 +1,14 @@
+#ifndef OPTION_HPP
+#define OPTION_HPP
+
+#include <iostream>
+
+class Option {
+public:
+	bool nullMovePrunningEnable;
+public:
+	Option();
+	void print();
+};
+
+#endif
