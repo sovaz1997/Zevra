b347b075d7812c1fe2d43c5b0ca206c0cc33a871
sovaz1997 <sovaz1997@yandex.ru>
2016-11-22 14:50:10 +0300

    last upd.

--- constants.cpp
+++ constants.cpp
@@ -1,4 +1,4 @@
 #include "constants.hpp"
 
-const uint64_t evalute_type_cutter = 1152921504606846976;
-const uint64_t evalute_cutter = (UINT64_MAX ^ evalute_type_cutter);
+//const uint64_t evalute_type_cutter = 1152921504606846976;
+//const uint64_t evalute_cutter = (UINT64_MAX ^ evalute_type_cutter);
