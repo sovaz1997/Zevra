218b290d4a158c2ca44dd65596ae1a92cfc79b99
sovaz1997 <sovaz1997@yandex.ru>
2016-11-24 15:27:11 +0300

    update nullmove prunning

--- search.cpp
+++ search.cpp
@@ -98,9 +98,11 @@ int64_t Game::negamax(BitBoard & b, int64_t alpha, int64_t beta, int depth, int 
 
 	if(option.nullMovePrunningEnable) {
 		if(!inNullMove && !b.inCheck(color) && !extended && !b.attacked && real_depth > 2 && b.getFiguresCount() > 3) {
-			if(negamax(b, beta - 1, beta, depth - 3, real_depth + 1, rule, true, true, pline) >= beta) {
+			b.whiteMove = !b.whiteMove;
+			if(-negamax(b, -beta, -alpha, depth - 3, real_depth + 1, rule, true, true, pline) >= beta) {
 				return beta;
 			}
+			b.whiteMove = !b.whiteMove;
 		}
 	}
 
