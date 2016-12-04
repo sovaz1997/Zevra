82d9ff9e77cea9c9c83d801dfa5b351a17af7ca1
Олег Смирнов < >
2016-12-04 10:32:41 +0300

    v1.2.1 r290 Release

--- uci.cpp
+++ uci.cpp
@@ -131,6 +131,6 @@ bool Game::uciHandler(std::string str) {
 }
 
 void Game::idPrint() {
-	std::cout << "id name Zevra v1.2.1 r272" << std::endl;
+	std::cout << "id name Zevra v1.2.1 r290" << std::endl;
 	std::cout << "id author sovaz1997" << std::endl;
 }
