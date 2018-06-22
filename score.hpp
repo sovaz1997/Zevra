e88912a0aa6b621796c57f2cbeec4ef3fcf33a8d
sovaz1997 <sovaz1997@yandex.ru>
2018-06-22 23:25:14 +0300

    Оптимизация по скорости Score с помощью внутреннего кеша оценки, добавление оценки mg-eg для пешек, исправление ошибок
    ELO   | 11.99 +- 9.07 (95%)\nSPRT  | 3.0+0.02s Threads=1 Hash=16MB\nLLR   | 2.21 (-2.20, 2.20) [0.00, 5.00]\nGames | N: 4523 W: 1897 L: 1741 D: 885\n\nELO   | 18.12 +- 12.94 (95%)\nSPRT  | 15.0+0.05s Threads=1 Hash=32MB\nLLR   | 1.79 (-2.20, 2.20) [0.00, 5.00]\nGames | N: 2054 W: 815 L: 708 D: 531

--- score.hpp
+++ score.hpp
@@ -1,11 +1,14 @@
 #ifndef SCORE_HPP
 #define SCORE_HPP
 
+#include <vector>
+
 class Score {
 public:
     int mg, eg;
     Score(int _mg, int _eg);
-    int getScore(int stageGame) const;
+    int getScore(double stageGame) const;
+    std::vector<int>scoreCash;
 };
 
 #endif
